14f8175ec411e727494cad8a95d6ebfbe7fce68e
NaohiroUesugi <4norahoi7@gmail.com>
2022-11-10 14:46:13 +0900

    fix: fix readme

--- src/common-concepts/main.rs
+++ src/common-concepts/main.rs
@@ -29,4 +29,28 @@ fn sandbox_variables() {
     println!("The value of shadow is: {}", shadow);
 }
 
-fn sandbox_data_tyles() {}
+fn sandbox_data_tyles() {
+    // 不動小数点
+    let _x = 2.0; // f64が基準
+
+    let _y: f32 = 3.0; // f32
+
+    // 数値演算
+    let sum = 5 + 10; // 足k算
+    let difference = 95.5 - 4.3; // 引き算
+    let product = 4 * 30; // 掛け算
+    let quotient = 56.7 / 32.2; // 割り算
+    let floored = 2 / 3; // 結果は0
+    let remainder = 43 % 5; // 余り
+    println!("sum is: {}", sum);
+    println!("difference is: {}", difference);
+    println!("product is: {}", product);
+    println!("quotient is: {}", quotient);
+    println!("floored is: {}", floored);
+    println!("remainder is: {}", remainder);
+
+    // 論理値型
+    let _t = true;
+
+    let _f: bool = false; // 明示的型注釈�きで
+}
